a4b69b56c084dbb9f0c7295bd49794e04052bbe7
alessandro <alessandrodamata6@gmail.com>
2023-07-01 20:21:10 -0300

    Configurando o contexo da palicação para que possa fazer a alteração do tema de light para dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { ThemeProvider } from "@mui/material";
 import { AppRoutes } from "./routes";
-import { LightTheme } from "./shared/themes";
+import { AppThemeProvider } from "./shared/contexts/ThemeContext";
+
 export function App() {
 
     return (
-        <ThemeProvider theme={LightTheme}>
+        <AppThemeProvider>
             <AppRoutes />
-        </ThemeProvider>
-
+        </AppThemeProvider>
     );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route, Navigate} from "react-router-dom";
 import {Button} from "@mui/material";
+import { useAppThemecontext } from "../shared/contexts/ThemeContext";
 
 export function AppRoutes(){
+
+    const {toggleTheme} = useAppThemecontext();
+
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">TESTE</Button>} />
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleTheme}>Toggle Theme</Button>} />
 
             <Route path="*"  element={<Navigate to={"/pagina-inicial"} />}/>
 
